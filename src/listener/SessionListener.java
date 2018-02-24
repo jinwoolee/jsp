c3713c46c29a34e44f69e5728562a52990b60da7
jw <jwlee1728@gmail.com>
2018-02-24 16:42:59 +0900

    HttpSessionAttributeListener

--- src/listener/SessionListener.java
+++ src/listener/SessionListener.java
@@ -1,9 +1,11 @@
 package listener;
 
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
-public class SessionListener implements HttpSessionListener {
+public class SessionListener implements HttpSessionListener, HttpSessionAttributeListener {
 
     public void sessionCreated(HttpSessionEvent se)  { 
     		
@@ -13,5 +15,23 @@ public class SessionListener implements HttpSessionListener {
     public void sessionDestroyed(HttpSessionEvent se)  {
     		//System.out.println("test : " + se.getSession().getAttribute("test"));
     		//System.out.println(se.getSession().getId() + "소멸");
-    }	
+    }
+
+	@Override
+	public void attributeAdded(HttpSessionBindingEvent event) {
+		
+		
+	}
+
+	@Override
+	public void attributeRemoved(HttpSessionBindingEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void attributeReplaced(HttpSessionBindingEvent event) {
+		// TODO Auto-generated method stub
+		
+	}	
 }

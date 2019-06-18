bf1f1be89916384be826965603806024d039898c
jw <jwlee1728@gmail.com>
2019-06-18 12:18:26 +0900

    bootstrap sesseion check filter 제외 조건 추가

--- src/main/java/kr/or/ddit/filter/LoginCheckFilter.java
+++ src/main/java/kr/or/ddit/filter/LoginCheckFilter.java
@@ -33,7 +33,7 @@ public class LoginCheckFilter implements Filter {
 		
 		//세션이 없어도 처리가 되어야 되는 것들 : /login, /js, /css, /img (.js, .css, .png, .gif)
 		if(uri.startsWith("/login") || uri.startsWith("/js") ||
-		   uri.startsWith("/css") || uri.startsWith("/img"))
+		   uri.startsWith("/css") || uri.startsWith("/img") || uri.startsWith("/bootstrap"))
 			chain.doFilter(request, response);
 		//session을 체크 해야하는 대상들
 		else if(req.getSession().getAttribute("USER_INFO") != null){

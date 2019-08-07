ea055f18b97901614678eb15a052a00574d51148
jwlee <jwlee.lecture@gmail.com>
2019-08-07 17:13:33 +0900

    1.application 기본객체 	- server info 	- url -> 물리적경로 조회
    2.sumCalculation (web flow)
    
    3.context-param
      init-param
    
    4. jsp servlet, default servlet

--- src/main/java/kr/or/ddit/servlet/HelloServlet.java
+++ src/main/java/kr/or/ddit/servlet/HelloServlet.java
@@ -3,6 +3,7 @@ package kr.or.ddit.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -30,8 +31,8 @@ public class HelloServlet extends HttpServlet{
 	private Logger logger = LoggerFactory.getLogger(HelloServlet.class);
 	
 	@Override
-	public void init() throws ServletException {
-		logger.debug("HelloServlet init()");
+	public void init(ServletConfig config) throws ServletException {
+		logger.debug("HelloServlet init() : {} ", config.getInitParameter("test"));
 		//System.out.println("HelloServlet init()");
 	}
 
